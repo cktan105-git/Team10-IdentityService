d3637f8eba23bf20276ea400ed3cfd458f9bcb9f
Keoh <weipien.keoh@gmail.com>
2021-04-09 19:54:19 +0800

    Service check in

---
+++ AspNetCoreWebService/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebService.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+        public string PostalCode { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public bool IsProducer { get; set; }
+        //public UserType UserType { get; set; }
+    }
+
+    /*public class UserType
+    {
+        public long UserTypeId { get; set; }
+        public string UserTypeDescription { get; set; }
+    }*/
+}
